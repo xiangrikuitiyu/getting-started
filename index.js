1c3d4a73e4a69097e439c055b018321612303027
xiangrikuitiyu <290942148@qq.com>
2025-04-21 14:02:23 +0800

    feat: add index.js for bot startup

---
+++ index.js
@@ -0,0 +1,17 @@
+import { WechatyBuilder } from 'wechaty'
+import { PuppetPadlocal } from 'wechaty-puppet-padlocal'
+
+const bot = WechatyBuilder.build({
+  puppet: new PuppetPadlocal({
+    token: process.env.PADLOCAL_TOKEN
+  }),
+})
+
+bot
+  .on('scan', (qrcode) => {
+    console.log(`Scan QR to login: https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`)
+  })
+  .on('login', user => console.log(`User ${user} logged in`))
+  .on('message', message => console.log(`Message: ${message}`))
+
+bot.start()
